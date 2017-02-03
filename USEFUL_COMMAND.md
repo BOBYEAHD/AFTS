9893faf0ab7b577afd368ed771d7724c32c45f44
strengthening <ducg@foxmail.com>
2017-02-03 14:29:40 +0800

    add

--- USEFUL_COMMAND.md
+++ USEFUL_COMMAND.md
@@ -1,3 +1,3 @@
-<h1>ipython 使用命令行</h1>
+<h1>ipython 实用命令行</h1>
 
 <p>导出历史命令行 <em>%history -g -f filename</em></p>
--- chapter1/example1.2.py
+++ chapter1/example1.2.py
@@ -1,13 +1,47 @@
+#-*- coding:utf-8 -*-
+from __future__ import division
 
 
 import pandas as pd
+import math
+import moment
 
-da = pd.read_table("./chapter1/text/d-ibm3dx7008.txt",index_col=['Date'])
+df = pd.read_table("./chapter1/text/d-ibm3dx7008.txt")
 
+df.columns = df.columns.map(lambda x :x.strip()) # 去掉字段名存在的空格
 
+df['Date'] = df['Date'].apply( lambda x : moment.date( str(x) ,'YYYYMMDD').date)
 
-da.head(1)
+df = df.set_index(df['Date'])
 
-da.tail(1)
+print df
 
-print da.tail(1)
+df.shape  
+
+df.head(1) # 数据集的第一行
+
+df.tail(1) # 数据集的最后一行 
+
+ibm = df['rtn']
+
+sibm = ibm * 100
+
+sibm.mean()
+
+sibm.median()
+
+sibm.sum()
+
+sibm.var()
+
+sibm.std()
+
+sibm.skew() # 偏度
+
+sibm.kurtosis() # 超额峰度
+
+s1 = sibm.skew()
+
+t1 = s1/math.sqrt(6/9845) # 检验统计量
+
+print t1
